8da847cb733224c7cf84bcbfd3f7e26473d53dd6
Michaelz <michael@cashboxng.com>
2019-12-05 15:06:39 +0100

    jgjg

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -10,10 +10,13 @@ const mongoose = require("mongoose");
   useFindAndModify: false
   "mongodb+srv://michaelztaskapp:123999abc@cluster0-4sjnt.mongodb.net/cashbox-api",
 }); */
-mongoose.connect("mongodb://127.0.0.1:27017/withdraw", {
-  useNewUrlParser: true,
-  //ds facilitates mongoose-mongodb connection so we can quickly access d data we want to access.
-  useCreateIndex: true,
-  //  correcting findAndModify deprecation warning
-  useFindAndModify: false
-});
+mongoose.connect(
+  "mongodb+srv://michaelztaskapp:123999abc@cluster0-4sjnt.mongodb.net/withdraw",
+  {
+    useNewUrlParser: true,
+    //ds facilitates mongoose-mongodb connection so we can quickly access d data we want to access.
+    useCreateIndex: true,
+    //  correcting findAndModify deprecation warning
+    useFindAndModify: false
+  }
+);
