5e862280baf5e91f25f41e43c339a6c847e318a3
Michaelz <michael@cashboxng.com>
2019-12-05 15:40:12 +0100

    Init

--- src/app.js
+++ src/app.js
@@ -7,6 +7,17 @@ require("./db/mongoose");
 const express = require("express");
 const app = express();
 
+//Enable CORS for all HTTP methods
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json()); //passing json to obj automatically
 app.use(withdrawalRouter); //registering our withdrawalRouter
 
