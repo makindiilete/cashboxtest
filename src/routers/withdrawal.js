1d5d84825c203b7ce22208099cc3b84053717f46
Michaelz <michael@cashboxng.com>
2019-12-06 11:27:17 +0100

    Delete route fixed

--- src/routers/withdrawal.js
+++ src/routers/withdrawal.js
@@ -39,6 +39,7 @@ router.post("/withdrawals", async (req, res) => {
 });*/
 
 router.delete("/withdrawals/:id", async (req, res) => {
+  const withdrawal = await Withdrawal.findById(req.params.id);
   if (withdrawal.status === "approved") {
     return res
       .status(400)
